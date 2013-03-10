24b1c3b340cbf1f4ea08298b4253c77b098db986
libertyernie <libertyernie0@gmail.com>
2013-03-10 09:10:56 -0500

    Get RSBE01 location from Options (and other changes)

--- Program.cs
+++ Program.cs
@@ -7,44 +7,48 @@ namespace TransferSSS {
 	class Program {
 		[STAThread]
 		static void Main(string[] args) {
-			ResourceNode toBrres_node = null;
-			try {
-				toBrres_node = NodeFactory.FromFile(null, "MiscData[80].brres");
-			} catch (FileNotFoundException) {
-				MessageBox.Show("Error: cannot find a MiscData[80].brres file to use as a base.");
-				return;
-			}
 			Options o = new Options();
 			if (o.ShowDialog() == DialogResult.OK) {
-				/*MessageBox.Show(
-					o.Copy_std + "," +
-					o.Prevbase_width_std + "," +
-					o.Prevbase_height_std + "," +
-					o.Copy_exp + "," +
-					o.Prevbase_width_exp + "," +
-					o.Prevbase_height_exp + "," +
-					o.Frontstname_width + "," +
-					o.Frontstname_height);*/
-
-				if (o.AddCodes) GCT.add("codes/RSBE01.gct", "Codeset.txt", "RSBE01.gct");
-
-				if (o.Common5 != null) {
-					ResourceNode fromBrres_common5 = NodeFactory.FromFile(null, o.Common5.FullName);
-					ResourceNode fromBrres_common5_node = fromBrres_common5.FindChild("sc_selmap_en", false).FindChild("MiscData[80]", false);
-					SSS.copy(fromBrres_common5_node, toBrres_node, o);
-					fromBrres_common5_node.ReplaceRaw(toBrres_node.WorkingSource.Address, toBrres_node.WorkingSource.Length);
-					fromBrres_common5.Merge();
-					fromBrres_common5.Export("common5.pac");
-				}
-				if (o.Mu_menumain != null) {
-					ResourceNode fromBrres_mu_menumain = NodeFactory.FromFile(null, o.Mu_menumain.FullName);
-					ResourceNode fromBrres_mu_menumain_node = fromBrres_mu_menumain.FindChild("MiscData[0]", false);
-					if (o.Common5 == null) {
-						SSS.copy(fromBrres_mu_menumain_node, toBrres_node, o);
+				if (o.Copy_std || o.Copy_exp) {
+					if (!new FileInfo("MiscData[80].brres").Exists) {
+						MessageBox.Show("Error: cannot find a MiscData[80].brres file to use as a base.");
+						return;
 					}
-					fromBrres_mu_menumain_node.ReplaceRaw(toBrres_node.WorkingSource.Address, toBrres_node.WorkingSource.Length);
-					fromBrres_mu_menumain.Merge();
-					fromBrres_mu_menumain.Export("mu_menumain.pac");
+					if (o.Common5 == null && o.Mu_menumain == null) {
+						MessageBox.Show("Error: no common5 or mu_menumain found to replace icons in.");
+						return;
+					}
+				}
+				if (o.RSBE01 != null) {
+					if (!new FileInfo("Codeset.txt").Exists) {
+						MessageBox.Show("Error: cannot find a Codeset.txt file to add to the GCT.");
+						return;
+					}
+				}
+
+				ResourceNode toBrres_node = NodeFactory.FromFile(null, "MiscData[80].brres");
+
+				if (o.AddCodes) GCT.add(o.RSBE01.FullName, "Codeset.txt", "RSBE01.gct");
+
+				if (o.Copy_std || o.Copy_exp) {
+					if (o.Common5 != null) {
+						ResourceNode fromBrres_common5 = NodeFactory.FromFile(null, o.Common5.FullName);
+						ResourceNode fromBrres_common5_node = fromBrres_common5.FindChild("sc_selmap_en", false).FindChild("MiscData[80]", false);
+						SSS.copy(fromBrres_common5_node, toBrres_node, o);
+						fromBrres_common5_node.ReplaceRaw(toBrres_node.WorkingSource.Address, toBrres_node.WorkingSource.Length);
+						fromBrres_common5.Merge();
+						fromBrres_common5.Export("common5.pac");
+					}
+					if (o.Mu_menumain != null) {
+						ResourceNode fromBrres_mu_menumain = NodeFactory.FromFile(null, o.Mu_menumain.FullName);
+						ResourceNode fromBrres_mu_menumain_node = fromBrres_mu_menumain.FindChild("MiscData[0]", false);
+						if (o.Common5 == null) {
+							SSS.copy(fromBrres_mu_menumain_node, toBrres_node, o);
+						}
+						fromBrres_mu_menumain_node.ReplaceRaw(toBrres_node.WorkingSource.Address, toBrres_node.WorkingSource.Length);
+						fromBrres_mu_menumain.Merge();
+						fromBrres_mu_menumain.Export("mu_menumain.pac");
+					}
 				}
 
 				MessageBox.Show("Remember to copy the new files to the correct locations.", "Finished");
